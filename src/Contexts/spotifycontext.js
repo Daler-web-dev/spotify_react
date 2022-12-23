80856152ca643a6c8bed39e70e192fef2f5b2da5
Daler <dsarifkulo021@gmail.com>
2022-12-23 20:33:59 +0500

    changed redirect url

--- src/Contexts/spotifycontext.js
+++ src/Contexts/spotifycontext.js
@@ -2,7 +2,7 @@ import { createContext } from "react";
 
 const spotify = createContext({
     client_id: "f2e286ece2574ad6b334b55d03764483",
-    REDIRECT_URI: "http://localhost:3000",
+    REDIRECT_URI: "https://63a5c97198831415ce0aeda9--tiny-praline-96ec74.netlify.app/",
     AUTH_ENDPOINT: "https://accounts.spotify.com/authorize",
     RESPONSE_TYPE: "token",
     token: ""
