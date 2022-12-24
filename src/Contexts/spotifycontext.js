e906dc58fa82f11beec9af6498b0e47c2935a549
Daler <dsarifkulo021@gmail.com>
2022-12-24 22:03:58 +0500

    fixed uri

--- src/Contexts/spotifycontext.js
+++ src/Contexts/spotifycontext.js
@@ -2,8 +2,8 @@ import { createContext } from "react";
 
 const spotify = createContext({
     client_id: "f2e286ece2574ad6b334b55d03764483",
-    REDIRECT_URI: "https://63a5c97198831415ce0aeda9--tiny-praline-96ec74.netlify.app/",
-    AUTH_ENDPOINT: "https://weprospoty.netlify.app/",
+    REDIRECT_URI: "https://weprospoty.netlify.app/",
+    AUTH_ENDPOINT: "https://accounts.spotify.com/authorize",
     RESPONSE_TYPE: "token",
     token: ""
 })
