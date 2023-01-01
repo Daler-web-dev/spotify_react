35a04b46d3b0493282a889885e3157bab0dbfd0c
Daler <dsarifkulo021@gmail.com>
2023-01-01 20:42:01 +0500

    col chnaged to two in mobile version

--- src/Components/Playlistlist.jsx
+++ src/Components/Playlistlist.jsx
@@ -14,7 +14,7 @@ const PlaylistList = ({plaslists, errorMsg = "", title}) => {
 					See all
 				</a>
 			</div>
-			<div className="max-2xl:grid-cols-5 max-md:grid-cols-3 max-sm:grid-cols-2 max-[400px]:grid-cols-1 w-full max-w-[1400px] mt-5 grid grid-cols-5 gap-6">
+			<div className="max-2xl:grid-cols-5 max-md:grid-cols-3 max-sm:grid-cols-2 max-[400px]:grid-cols-2 w-full max-w-[1400px] mt-5 grid grid-cols-5 gap-6">
 				{
 					errorMsg.length > 0 ? <span className="text-[red] text-[24px]" >{errorMsg}</span> :
 					plaslists.map(item => <PlaylistCard key={item.id} item={item} />)
